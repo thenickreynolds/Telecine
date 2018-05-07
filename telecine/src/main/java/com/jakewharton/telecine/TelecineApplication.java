0bdc3cee175a80ac381c5ebadab670ba19812941
Nick Reynolds <me@nickreynolds.net>
2018-05-06 18:25:21 -0700

    Disable bugsnag

--- telecine/src/main/java/com/jakewharton/telecine/TelecineApplication.java
+++ telecine/src/main/java/com/jakewharton/telecine/TelecineApplication.java
@@ -25,17 +25,17 @@ public final class TelecineApplication extends Application
     if (BuildConfig.DEBUG) {
       Timber.plant(new Timber.DebugTree());
     } else {
-      Bugsnag.init(this, BuildConfig.BUGSNAG_KEY);
-      Bugsnag.setReleaseStage(BuildConfig.BUILD_TYPE);
-      Bugsnag.setProjectPackages("com.jakewharton.telecine");
-
+//      Bugsnag.init(this, BuildConfig.BUGSNAG_KEY);
+//      Bugsnag.setReleaseStage(BuildConfig.BUILD_TYPE);
+//      Bugsnag.setProjectPackages("com.jakewharton.telecine");
+//
       final BugsnagTree tree = new BugsnagTree();
-      Bugsnag.getClient().beforeNotify(new BeforeNotify() {
-        @Override public boolean run(Error error) {
-          tree.update(error);
-          return true;
-        }
-      });
+//      Bugsnag.getClient().beforeNotify(new BeforeNotify() {
+//        @Override public boolean run(Error error) {
+//          tree.update(error);
+//          return true;
+//        }
+//      });
 
       Timber.plant(tree);
     }
